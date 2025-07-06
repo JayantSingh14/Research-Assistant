bc7a81ca410eb651268ba0e5b9c5e1ed4ca44fc2
JayantSingh14 <jayantpratap100@gmail.com>
2025-07-06 21:47:10 +0530

    Initial commit of ResearchAssistance project

---
+++ src/test/java/com/research/SmartAssistant/SmartAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.SmartAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
